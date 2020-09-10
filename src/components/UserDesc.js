52a2ce133a9967c19277b0efa03f7028cdb0d2d6
Sebastian Breit <flockean11@gmail.com>
2020-09-10 08:52:55 +0200

    ui improvements

--- src/components/UserDesc.js
+++ src/components/UserDesc.js
@@ -12,6 +12,7 @@ const UserDesc = ({ data, setIsOpen }) => {
     name,
     posts,
     images,
+    followedBy,
   } = data?.user;
 
   const onClick = () => {
@@ -22,20 +23,31 @@ const UserDesc = ({ data, setIsOpen }) => {
   };
   return (
     <div className="user-desc">
-      <h2 className="name">{name}</h2>
-      <h3 className="desc">{desc}</h3>
+      {name && <h2 className="name">{name}</h2>}
+      {desc && <h3 className="desc">{desc}</h3>}
       {link && <a href={link}>{linkText}</a>}
-      <h4 className="followers">
-        <span>{followers}</span> followers
-      </h4>
-      <h4 className="following">
-        following <span>{following}</span> users
-      </h4>
-      <h4 className="posts">
-        <span>{posts}</span> posts
-      </h4>
-      <button onClick={onClick}>Reload</button>
-      <UserImages images={images} />
+      {followers && (
+        <h4 className="followers">
+          <span>{followers}</span>{" "}
+          {followers !== "1" ? "followers" : "follower"}
+        </h4>
+      )}
+      {following && (
+        <h4 className="following">
+          following <span>{following}</span>{" "}
+          {following !== "1" ? "users" : "user"}
+        </h4>
+      )}
+      {posts && (
+        <h4 className="posts">
+          <span>{posts}</span> {posts !== "1" ? "posts" : "post"}
+        </h4>
+      )}
+      {followedBy && <h4 className="followed-by">{followedBy}</h4>}
+      <button className="reload-button" onClick={onClick}>
+        Reload
+      </button>
+      {images && <UserImages images={images} />}
     </div>
   );
 };
