90fad8bc03957b3c33b77debde19ba3c308f1e1d
Sebastian Breit <flockean11@gmail.com>
2020-09-14 07:06:57 +0200

    improvements

--- src/components/UserImage.js
+++ src/components/UserImage.js
@@ -3,7 +3,13 @@ import "./UserImage.css";
 
 const UserImage = ({ img: { href, alt, srcSet } }) => {
   return (
-    <a target="_blank" rel="noopener noreferrer" href={href} key={href}>
+    <a
+      className="user-image-link"
+      target="_blank"
+      rel="noopener noreferrer"
+      href={href}
+      key={href}
+    >
       <img className="user-image" alt={alt} title={alt} srcSet={srcSet} />
     </a>
   );
